94b21d151d6d0e2fcf98a9e749c10ded24a0aadb
macphersoncole <cole.e.macpherson@gmail.com>
2021-10-28 16:09:27 -0600

    Cleaned weather data

--- Code/cleaning_data.m
+++ Code/cleaning_data.m
@@ -39,3 +39,26 @@ drought_cleaned.HighestDroughtLevel = temp;
 writetable(drought_cleaned,'Data\Clean Data\Drought\USDM-colorado_drought_weekly_2001_2021_CLEANED.csv');
 
 %% Weather Data Cleaning
+base = 'Data\Raw Data\Weather';
+list = dir(fullfile(base, '*.xlsx'));
+weatherVals = zeros(4017,36);
+for i = 1:numel(list)
+    file = fullfile(base, list(i).name);
+    result = readtable(file);
+    weatherVals(:,(((i-1)*4)+1):(i*4)) = result{:,2:5};
+    dates = result(:,1);
+end
+
+% find average values across eacch day
+weather_avg_daily = zeros(4017,4);
+for i = 1:length(weatherVals)
+    weather_avg_daily(i,1) = mean(weatherVals(i,(1:4:36)),'omitnan');
+    weather_avg_daily(i,2) = mean(weatherVals(i,(2:4:36)),'omitnan');
+    weather_avg_daily(i,3) = mean(weatherVals(i,(3:4:36)),'omitnan');
+    weather_avg_daily(i,4) = mean(weatherVals(i,(4:4:36)),'omitnan');
+end
+
+% put back in table format
+weather_clean = array2table(weather_avg_daily);
+weather_clean = [dates weather_clean];
+weather_clean.Properties.VariableNames = {'Dates', 'Max Temp', 'Min Temp', 'Pcpn', 'Snow'};
