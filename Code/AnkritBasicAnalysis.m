57b079a64513afe77f46a89d08236765e62c1109
anup2318 <anup2318@colorado.edu>
2021-12-01 14:48:55 -0700

    Causes Bar Graph

--- Code/AnkritBasicAnalysis.m
+++ Code/AnkritBasicAnalysis.m
@@ -17,59 +17,67 @@ cause = table2array(cause);
 cause = string(cause);
 
 fprintf('There were %d fires that burned over 5000 acres \n', length(cause));
-noCause = find(cause == 'No cause indicated');
-fprintf('%d fires had no cause indicated \n', length(noCause));
+NoCause = find(cause == 'No cause indicated');
+fprintf('%d fires had no cause indicated \n', length(NoCause));
 
 
 natural = find(cause == 'Natural');
 NATURAL = find(cause == 'NATURAL');
+naturalVec = [natural; NATURAL];
+Natural = length(NATURAL) + length(natural);
 fprintf('%d fires were due to natural causes \n', length(NATURAL) + length(natural));
 
 
 oneLightning = find(cause == '1 - Lightning');
 lightning = find(cause == 'Lightning');
+lightningVec = [oneLightning; lightning];
+Lightning = length(oneLightning) + length(lightning);
 fprintf('%d fires were due to Lightning \n', length(oneLightning) + length(lightning));
 
-human = find(cause == 'Human');
-fprintf('%d fires were due to humans \n', length(human));
+Human = find(cause == 'Human');
+fprintf('%d fires were due to humans \n', length(Human));
 
 
-unknown = find(cause == 'Unknown');
-fprintf('%d fires had an unkown cause \n', length(unknown));
+Unknown = find(cause == 'Unknown');
+fprintf('%d fires had an unkown cause \n', length(Unknown));
 
 miscellaneous = find(cause == '9 - Miscellaneous');
 miscellaneousTwo = find(cause == 'Miscellaneous');
+Miscellaneous = [miscellaneous; miscellaneousTwo];
 fprintf('%d fires had a miscellaneous cause \n', length(miscellaneous) + length(miscellaneousTwo));
 
-undetermined = find(cause == 'Undetermined');
-fprintf('%d fires had an undetermined cause \n', length(undetermined));
+Undetermined = find(cause == 'Undetermined');
+fprintf('%d fires had an undetermined cause \n', length(Undetermined));
 
-fourCampfire = find(cause == '4 - Campfire');
-fprintf('%d fires were caused by a campfire \n', length(fourCampfire));
+Campfire = find(cause == '4 - Campfire');
+fprintf('%d fires were caused by a campfire \n', length(Campfire));
 
-missing = find(cause == 'Missing/Undefined');
-fprintf('%d fires had causes that were Missing/Undefined \n', length(missing));
+Missing = find(cause == 'Missing/Undefined');
+fprintf('%d fires had causes that were Missing/Undefined \n', length(Missing));
 
-equipment = find(cause == '2 - Equipment use');
-fprintf('%d fires were caused by equipment use \n', length(equipment));
+Equipment = find(cause == '2 - Equipment use');
+fprintf('%d fires were caused by equipment use \n', length(Equipment));
 
-arson = find(cause == '7 - Arson');
-fprintf('%d fires were caused by arson \n', length(arson));
+Arson = find(cause == '7 - Arson');
+fprintf('%d fires were caused by arson \n', length(Arson));
 
-smoking = find(cause == '3 - Smoking');
-fprintf('%d fires were caused by smoking \n', length(smoking));
+Smoking = find(cause == '3 - Smoking');
+fprintf('%d fires were caused by smoking \n', length(Smoking));
 
-structure = find(cause == 'Structure Fire');
-fprintf('%d fires were caused by a Structure Fire \n', length(structure)); 
+Structure = find(cause == 'Structure Fire');
+fprintf('%d fires were caused by a Structure Fire \n', length(Structure)); 
 
 
-total = length(fourCampfire) + length(undetermined) + length(unknown) + length(lightning) + length(human) + length(oneLightning) + length(natural) + length(NATURAL) + length(noCause) + length(miscellaneous) + length(equipment) + length(arson) + length(miscellaneousTwo) + length(smoking) + length(structure) + length(missing); 
-% 
-% for i = 1:length(cause)
-%     if cause(i) ~= '7 - Arson' && cause(i) ~= '2 - Equipment use' && cause(i) ~= '4 - Campfire' && cause(i) ~= 'Undetermined' && cause(i) ~= '9 - Miscellaneous' && cause(i) ~= 'Unknown' && cause(i) ~= 'Lightning' && cause(i) ~= '1 - Lightning' && cause(i) ~= 'Human' && cause(i) ~= 'Natural' && cause(i) ~= 'NATURAL' && cause(i) ~= 'No cause indicated'
-%         fprintf('%s', cause(i));
-%         i
-%     end
-%     
-% end
-plot1 = histogram(Acres);
+total = length(Campfire) + length(Undetermined) + length(Unknown) + length(lightning) + length(Human) + length(oneLightning) + length(natural) + length(NATURAL) + length(NoCause) + length(miscellaneous) + length(Equipment) + length(Arson) + length(miscellaneousTwo) + length(Smoking) + length(Structure) + length(Missing); 
+
+humanCauseVec = [Arson; Human; Smoking; Structure; Equipment];
+%Missing = Missing';
+%Miscellaneous = Miscellaneous';
+UnknownVec = [Unknown; Undetermined; Missing; Miscellaneous];
+X = categorical({'Natural Causes', 'Lighting', 'Human', 'Uknown'});
+X = reordercats(X,{'Natural Causes', 'Lighting', 'Human', 'Uknown'});
+barGraph = [length(naturalVec), length(lightningVec), length(humanCauseVec), length(Unknown)];
+figure
+plot1 = bar(X, barGraph);
+ylabel('Number of Wildfires', 'FontWeight', 'bold');
+xlabel('Listed Cause', 'FontWeight', 'bold')
